b7c714c4e8e37f27308c1bda146a21263cff7cbc
Alvaro Argañaraz <you@example.com>
2020-08-19 21:27:52 -0300

    Updates

---
+++ precache-manifest.f277bfbaa519d68fc680db28c162ed6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c575c8edc7b47dced3f6adc51c1add",
+    "url": "/React-to-do-list/index.html"
+  },
+  {
+    "revision": "941199476d1ff54ade86",
+    "url": "/React-to-do-list/static/css/main.1f6deb75.chunk.css"
+  },
+  {
+    "revision": "13378cd698d80046920f",
+    "url": "/React-to-do-list/static/js/2.efdbadb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-to-do-list/static/js/2.efdbadb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941199476d1ff54ade86",
+    "url": "/React-to-do-list/static/js/main.88177584.chunk.js"
+  },
+  {
+    "revision": "8789ccc5d5a7e26eb39b",
+    "url": "/React-to-do-list/static/js/runtime-main.2f64c7ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
